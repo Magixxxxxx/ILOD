0c89a957a8e8843f066f7bc825ff5591a308ce7f
Your Name <you@example.com>
2021-05-31 11:45:25 +0800

    comparemask

--- Analysis/test_model.py
+++ Analysis/test_model.py
@@ -44,15 +44,38 @@ def analyseParams(model, layer):
 def compareModels(model1, model2):
     state_dict1 = torch.load(model1, map_location=torch.device('cpu'))
     state_dict2 = torch.load(model2, map_location=torch.device('cpu'))
+
+
+    params1 = np.array([])
+    params2 = np.array([])
+    pre_l = '.'
+    cur_l = '.'
     for (name1,p1),(name2,p2) in zip(state_dict1['model'].items(),state_dict2['model'].items()):
         if 'mask' in name1:
-            active1 = p1.view(-1)>0.005
-            active2 = p2.view(-1)>0.005
-            common = [a and b for a,b in zip(active1, active2)]
-            com_by_all = sum(common).numpy() / len(p1.view(-1))
-            com_by_1s = sum(common).numpy() / sum(active1).numpy()
-            print("%s %.2f %.2f" % (name1, com_by_1s, com_by_all))
-            # print(sum(p1.view(-1)>0.005==p2.view(-1)>0.005), len(p1.view(-1)))
+
+            cur_l = name1[19]
+            if cur_l == pre_l:
+                active1 = np.array(p1.view(-1)>0.005, bool)
+                active2 = np.array(p2.view(-1)>0.005, bool)
+                params1 = np.hstack([params1,active1])
+                params2 = np.hstack([params2,active2])
+                continue
+
+            print("layer{}".format(pre_l), end=" ")
+            common = np.sum([a and b for a,b in zip(params1, params2)])
+            s_of_1 = np.sum(params1)
+            s_of_layer = len(params1)
+
+            com_by_1s = np.round(common / s_of_layer, 2)            
+            com_by_all = np.round(common / s_of_1, 2)
+
+            print("c_by_1s:{} c_by_all:{}".format(com_by_1s, com_by_all))        
+
+            pre_l = cur_l
+            params1 = np.array([])
+            params2 = np.array([])
+
+
 
 def compareLayers(model):
     state_dict = torch.load(model, map_location=torch.device('cpu'))
@@ -69,10 +92,12 @@ def compareLayers(model):
 
 if __name__ == '__main__':
 
-    model1 = "voc[16, 20]_pb[body]moco200_Adam:1e-4_1e-5/model_47.pth"
-    model2 = "voc[16, 20]_pb[body]moco2002_Adam:1e-4_1e-5/model_47.pth"
-    # path = "checkpoints/"
-    layer = "backbone.body.layer4.2.conv3.weight"
-    
-    compareModels(model1, model2)
+    for i in range(1, 8):
+        for j in range(i+1, 8):
+            print("compare {} {}".format(i, j))
+            if i == 4 or j == 4:
+                continue
+            model1 = "voc[{}, {}]_pb[body]_Adam:1e-4_1e-5/model_100.pth".format(i,i)
+            model2 = "voc[{}, {}]_pb[body]_Adam:1e-4_1e-5/model_100.pth".format(j,j)
+            compareModels(model1, model2)
 
