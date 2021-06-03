8e97cf0c1966760f568e328ce280cf56e65f3478
Your Name <you@example.com>
2021-06-03 15:33:24 +0800

    alltraintest2

--- rtx_run.sh
+++ rtx_run.sh
@@ -1,18 +1,18 @@
-ilod='[20, 20]'
+ilod='[1, 20]'
 optim='Adam'
 lr_weight=1e-4
 lr_mask=1e-5
 model='pb[body]'
-details='alltest'
+details=''
 out="voc${ilod}${details}_${model}"
 port=$(python -c 'import random;print(random.randrange(10000, 20000))')
 
-python -m torch.distributed.launch --nproc_per_node=4 --master_port ${port} train.py \
+python -m torch.distributed.launch --nproc_per_node=8 --master_port ${port} train.py \
 --data-path /root/userfolder/data/voc2007 \
 --dataset voc2007 --ilod "${ilod}" --num-classes 21 \
 --base-model models/resnet50-19c8e357.pth \
 --pb body --freeze None \
---epochs 48 --lr-steps 32 44 -b 2 \
+--epochs 36 --lr-steps 24 33 -b 4 \
 --lr-w ${lr_weight} --lr-m ${lr_mask} --optim ${optim} \
 --output-dir "${out}" \
 --mask-init 1s --mask-scale 1e-2 > $out'.out'
--- test.sh
+++ test.sh
@@ -1,11 +1,19 @@
-out='test'
+ilod='[1, 20]'
+optim='Adam'
+lr_weight=1e-4
+lr_mask=1e-5
+model='pb[body]'
+details='_[11, 20]train_remove'
+out="voc${ilod}${details}_${model}"
+port=$(python -c 'import random;print(random.randrange(10000, 20000))')
 
-python test.py \
---data-path /root/userfolder/data/VOC \
---dataset voc --ilod '[1, 10]' --num-classes 11 \
---base-model model/resnet50-19c8e357.pth \
---pb None --freeze None --pureFasterRCNN \
---epochs 18 --lr-steps 10 15 -b 2 \
---lr-w 1e-3 --lr-m 0 --optim 'SGD' \
+python -m torch.distributed.launch --nproc_per_node=4 --master_port ${port} train.py \
+--data-path /root/userfolder/data/voc2007 \
+--dataset voc2007 --ilod "${ilod}" --num-classes 21 \
+--base-model models/resnet50-19c8e357.pth \
+--pb body --freeze None \
+--epochs 48 --lr-steps 32 44 -b 4 \
+--lr-w ${lr_weight} --lr-m ${lr_mask} --optim ${optim} \
 --output-dir "${out}" \
---mask-init 1s --mask-scale 1e-2 > "${out}.out"
+--mask-init 1s --mask-scale 1e-2 > $out'.out' \
+--test-only --resume 'results/alltraintest-voc[11, 20]_pb[body]_Adam:1e-4_1e-5/model_18_0.688.pth'
